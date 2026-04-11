1fb356f3e56eb35d70bb6b9c042785d9e77e1248
pedroSimon2 <pedrosimon290193@gmail.com>
2026-04-11 17:40:08 +0200

    Config: Añadidas múltiples entradas para el build de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path' // Necesario para gestionar las rutas de los archivos
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        // Mantenemos el index y añadimos todas tus páginas secundarias
+        main: resolve(__dirname, 'index.html'),
+        nosotros: resolve(__dirname, 'nosotros.html'),
+        productos: resolve(__dirname, 'productos.html'),
+        contacto: resolve(__dirname, 'contacto.html'),
+        pociones: resolve(__dirname, 'pociones.html'),
+        ingredientes: resolve(__dirname, 'ingredientes.html'),
+        armaduras: resolve(__dirname, 'armaduras.html'),
+      }
+    }
+  }
 })
